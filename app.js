0d260c238c2768a5b782c0dc67fd68d9ac52a0d7
kadir-zeren <k4dzeren@gmail.com>
2024-04-17 14:03:43 +0300

    js intro

---
+++ app.js
@@ -0,0 +1,48 @@
+// console.log("Localde bulunan js calisti");
+
+// alert("Bu bir alert dir");
+
+// karar = confirm("Emin misiniz E/H");
+// console.log(karar);
+
+// console.warn("Bu bir uyaridir");
+// console.error("Eyvah hata olustu...");
+
+const pi = 3.14;
+console.log(pi);
+console.log(typeof pi);
+
+const sayi1 = "3";
+console.log(typeof sayi1);
+
+const number1 = 5;
+
+const varMi = true;
+const isOpen = false;
+console.log(isOpen, typeof isOpen);
+
+let language = "Java";
+console.log(language, typeof language);
+
+language = "Javascript";
+console.log(language);
+
+language = true;
+console.log(language);
+
+language = 3.14;
+console.log(language);
+
+let adet;
+console.log(adet);
+
+adet = "23";
+console.log(typeof adet);
+
+{
+  let LocalDegisken = 5;
+  LocalDegisken = LocalDegisken + 1;
+  console.log(LocalDegisken);
+}
+
+console.log(LocalDegisken);
